a7a5424e3a0920832829172d2dfbca756175b99a
jonathanothoudt <>
2021-03-05 16:22:32 -0600

    submit button and display

--- scripts/client.js
+++ scripts/client.js
@@ -2,4 +2,28 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('JQ');
+    $('#submitButton').on('click', calculateCosts);
 }
+
+function calculateCosts(){
+    console.log('in calculateCosts');
+let employee = {
+    firstName: $('#firstName').val(),
+    lastName: $('#lastName').val(),
+    idNumber: $('#idNumber').val(),
+    jobTitle: $('#jobTitle').val(),
+    annualSalary: $('#annualSalary').val()
+};//end object
+$('#empOut').append(`<tr>
+    <td>${employee.firstName}</td>
+    <td>${employee.lastName}</td>
+    <td>${employee.idNumber}</td>
+    <td>${employee.jobTitle}</td>
+    <td>${employee.annualSalary}</td>
+</tr>`);
+$('#firstName').val('');
+$('#lastName').val('');
+$('#idNumber').val('');
+$('#jobTitle').val('');
+$('#annualSalary').val('');
+}//end calculateCosts()
