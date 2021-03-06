06666d147e69ef759dda55ea147ee7c915a077ee
jonathanothoudt <>
2021-03-05 20:39:49 -0600

    Calculator working and css append on if statement

--- README.md
+++ README.md
@@ -6,4 +6,16 @@
 
 Your project description goes here. What problem did you solve? How did you solve it?
 
+[x] Inputs for employee first name, last name, id number, job title, annual salary
+
+[x] create a submit button
+
+[x]get information on click
+
+[x]store information
+
+[x]display information in the table
+
+[x]create delete button
+
 Additional README details can be found [here](https://github.com/PrimeAcademy/readme-template/blob/master/README.md).
--- scripts/client.js
+++ scripts/client.js
@@ -2,28 +2,46 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('JQ');
-    $('#submitButton').on('click', calculateCosts);
+    $('#submitButton').on('click', getEmployee);
 }
-
-function calculateCosts(){
-    console.log('in calculateCosts');
-let employee = {
+let employe= {};
+let employees =[];
+let totalMonthly=0;
+function getEmployee(){
+employee = {
     firstName: $('#firstName').val(),
     lastName: $('#lastName').val(),
     idNumber: $('#idNumber').val(),
     jobTitle: $('#jobTitle').val(),
     annualSalary: $('#annualSalary').val()
 };//end object
+employees.push(employee);
 $('#empOut').append(`<tr>
     <td>${employee.firstName}</td>
     <td>${employee.lastName}</td>
     <td>${employee.idNumber}</td>
     <td>${employee.jobTitle}</td>
-    <td>${employee.annualSalary}</td>
+    <td>${(employee.annualSalary)}</td>
+    <td><button id="deleteEmployee">Delete</button>
 </tr>`);
 $('#firstName').val('');
 $('#lastName').val('');
 $('#idNumber').val('');
 $('#jobTitle').val('');
 $('#annualSalary').val('');
+
+calculateCosts(employees);
 }//end calculateCosts()
+
+function calculateCosts(employees){
+    totalMonthly= 0;
+    for(let i=0; i<employees.length; i++){
+    employeeMonthly= employees[i].annualSalary/12;
+    totalMonthly += employeeMonthly;
+    $('#monthlyCost').empty()
+    $('#monthlyCost').append(Number(totalMonthly));
+    if(totalMonthly > 20000){
+        $('#monthlyCost').css('color', 'red');
+    }
+    }
+    }
