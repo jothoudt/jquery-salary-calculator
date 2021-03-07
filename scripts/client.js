7259af320b0d61049e3326f7dfb1ac71ae18e80b
jonathanothoudt <>
2021-03-07 11:21:15 -0600

    add a few more comments

--- scripts/client.js
+++ scripts/client.js
@@ -8,6 +8,8 @@ function onReady(){
 let employe= {};
 let employees =[];
 let totalMonthly=0;
+
+//function to get employee info
 function getEmployee(){
 employee = {
     firstName: $('#firstName').val(),
@@ -17,6 +19,7 @@ employee = {
     annualSalary: $('#annualSalary').val()
 };//end object
 employees.push(employee);
+//append info to dom
 $('#empOut').append(`<tr>
     <td>${employee.firstName}</td>
     <td>${employee.lastName}</td>
@@ -30,16 +33,19 @@ $('#lastName').val('');
 $('#idNumber').val('');
 $('#jobTitle').val('');
 $('#annualSalary').val('');
-
+//add employee info to calculator
 calculateCosts(employees);
 }//end calculateCosts()
 
+//calculate employee info
 function calculateCosts(employees){
     totalMonthly= 0;
+    //loop through employees
     for(let i=0; i<employees.length; i++){
     employeeMonthly= employees[i].annualSalary/12;
     totalMonthly += employeeMonthly;
     $('#monthlyCost').empty()
+    //append calculator total to dom
     $('#monthlyCost').append(Number(totalMonthly.toFixed(2)));
     if(totalMonthly > 20000){
         $('#monthlyCost').css('color', 'red');
@@ -47,12 +53,16 @@ function calculateCosts(employees){
     }//end for
 }//end calculate costs
 
+//function to remove employee
     function removeEmployee(){
         console.log('in removeEmployee');
+        //select item and remove from total
         itemToRemove =$(this).parent().parent().text().match(employee.annualSalary);
         totalMonthly -= itemToRemove/12 ;
+        //console.logs to troubleshoot function
         console.log(itemToRemove);
         console.log(totalMonthly);
+        //empty old total and add new total to the dom
         $('#monthlyCost').empty();
         $('#monthlyCost').append(Number(totalMonthly.toFixed(2)));
         $(this).parent().parent().remove();
